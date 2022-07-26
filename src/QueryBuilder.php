0101e7b2ff19216a8dcb7032833806c1c814961e
co0lc0der <c0der@ya.ru>
2022-07-26 16:09:45 +0500

    add like() method

--- src/QueryBuilder.php
+++ src/QueryBuilder.php
@@ -242,6 +242,22 @@ class QueryBuilder
 	}
 
 	/**
+	 * @param array|string $cond
+	 * @return $this
+	 */
+	public function like($cond = []): QueryBuilder
+	{
+		if ($cond) {
+			if (is_string($cond)) {
+				$this->where($cond);
+			} else if (is_array($cond)) {
+				$this->where([[$cond[0], 'LIKE', $cond[1]]]);
+			}
+		}
+		return $this;
+	}
+
+	/**
 	 * @param int $limit
 	 * @return $this
 	 */
