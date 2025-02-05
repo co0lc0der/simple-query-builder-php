b5fc299d842cc1a2f053450a7c87a3c7ea9253ac
co0lc0der <c0der@ya.ru>
2025-02-05 12:29:00 +0500

    update PHPDocs

--- src/QueryBuilder.php
+++ src/QueryBuilder.php
@@ -200,7 +200,7 @@ class QueryBuilder
 	/**
 	 * @param array|string $table
 	 * @param string $field
-	 * @return string|$this
+	 * @return QueryBuilder|array
 	 */
 	public function count($table, string $field = '')
 	{
@@ -221,7 +221,7 @@ class QueryBuilder
 
 	/**
 	 * @param string $column
-	 * @return QueryBuilder|string|array
+	 * @return QueryBuilder|array
 	 */
 	public function column(string $column = 'id')
 	{
@@ -236,6 +236,7 @@ class QueryBuilder
 		}
 
 		$this->query();
+
 		return array_column($this->result, $column);
 		//$this->query('', [], $column, self::FETCH_COLUMN);
 		//return $this->result;
@@ -268,6 +269,7 @@ class QueryBuilder
 		}
 
 		$this->query();
+
 		return array_column($this->result, $column, $key);
 	}
 
@@ -511,7 +513,7 @@ class QueryBuilder
 	 * @param array $params
 	 * @param int|string $column
 	 * @param int $fetch
-	 * @return $this
+	 * @return QueryBuilder
 	 */
 	public function query(string $sql = '', array $params = [], $column = 0, int $fetch = self::FETCH_ALL): QueryBuilder
 	{
@@ -556,12 +558,13 @@ class QueryBuilder
 		return $this;
 	}
 
-	/**
-	 * @param array|string $table
-	 * @param array|string $fields
-	 * @return $this
-	 */
-	public function select($table, $fields = '*', $dist = false): QueryBuilder
+    /**
+     * @param array|string $table
+     * @param array|string $fields
+     * @param bool $dist
+     * @return QueryBuilder
+     */
+	public function select($table, $fields = '*', bool $dist = false): QueryBuilder
 	{
 		if (empty($table) || empty($fields)) {
 			$this->setError('Empty $table or $fields in ' . __METHOD__);
@@ -598,7 +601,7 @@ class QueryBuilder
 	/**
 	 * @param array|string $where
 	 * @param string $addition
-	 * @return $this
+	 * @return QueryBuilder
 	 */
 	public function where($where, string $addition = ''): QueryBuilder
 	{
@@ -624,7 +627,7 @@ class QueryBuilder
 
 	/**
 	 * @param array|string $having
-	 * @return $this
+	 * @return QueryBuilder
 	 */
 	public function having($having): QueryBuilder
 	{
@@ -647,7 +650,7 @@ class QueryBuilder
 	/**
 	 * @param array|string $field
 	 * @param string $value
-	 * @return $this
+	 * @return QueryBuilder
 	 */
 	public function like($field, string $value = ''): QueryBuilder
 	{
@@ -670,7 +673,7 @@ class QueryBuilder
 	/**
 	 * @param array|string $field
 	 * @param string $value
-	 * @return $this
+	 * @return QueryBuilder
 	 */
 	public function notLike($field, string $value = ''): QueryBuilder
 	{
@@ -707,7 +710,7 @@ class QueryBuilder
 
 	/**
 	 * @param string $field
-	 * @return $this
+	 * @return QueryBuilder
 	 */
 	public function isNotNull(string $field): QueryBuilder
     {
@@ -722,7 +725,7 @@ class QueryBuilder
 
 	/**
 	 * @param string $field
-	 * @return $this
+	 * @return QueryBuilder
 	 */
 	public function notNull(string $field): QueryBuilder
     {
@@ -732,7 +735,7 @@ class QueryBuilder
 
 	/**
 	 * @param int $limit
-	 * @return $this
+	 * @return QueryBuilder
 	 */
 	public function limit(int $limit = 1): QueryBuilder
 	{
@@ -747,7 +750,7 @@ class QueryBuilder
 
 	/**
 	 * @param int $offset
-	 * @return $this
+	 * @return QueryBuilder
 	 */
 	public function offset(int $offset = 0): QueryBuilder
 	{
@@ -758,7 +761,7 @@ class QueryBuilder
 	/**
 	 * @param string|array $field
 	 * @param string $sort
-	 * @return $this
+	 * @return QueryBuilder
 	 */
 	public function orderBy($field = '', string $sort = ''): QueryBuilder
 	{
@@ -793,7 +796,7 @@ class QueryBuilder
 
 	/**
 	 * @param string|array $field
-	 * @return $this
+	 * @return QueryBuilder
 	 */
 	public function groupBy($field = ''): QueryBuilder
 	{
@@ -809,7 +812,7 @@ class QueryBuilder
 
 	/**
 	 * @param array|string $table
-	 * @return $this
+	 * @return QueryBuilder
 	 */
 	public function delete($table): QueryBuilder
 	{
@@ -835,7 +838,7 @@ class QueryBuilder
 	/**
 	 * @param array|string $table
 	 * @param array $fields
-	 * @return $this
+	 * @return QueryBuilder
 	 */
 	public function insert($table, array $fields = []): QueryBuilder
 	{
@@ -880,7 +883,7 @@ class QueryBuilder
 	/**
 	 * @param array|string $table
 	 * @param array $fields
-	 * @return $this
+	 * @return QueryBuilder
 	 */
 	public function update($table, array $fields = []): QueryBuilder
 	{
@@ -914,9 +917,9 @@ class QueryBuilder
 
 	/**
 	 * @param array|string $table
-	 * @param $on
+	 * @param array|string $on
 	 * @param string $join_type
-	 * @return $this
+	 * @return QueryBuilder
 	 */
 	public function join($table, $on, string $join_type = 'INNER'): QueryBuilder
 	{
@@ -963,7 +966,7 @@ class QueryBuilder
 
     /**
      * @param bool $unionAll
-     * @return $this
+     * @return QueryBuilder
      */
     public function union(bool $unionAll = false): QueryBuilder
     {
@@ -973,7 +976,7 @@ class QueryBuilder
     }
 
     /**
-     * @return $this
+     * @return QueryBuilder
      */
     public function unionAll(): QueryBuilder
     {
@@ -985,17 +988,12 @@ class QueryBuilder
     /**
      * @param string|array $table
      * @param bool $unionAll
-     * @return $this
+     * @return QueryBuilder
      */
     public function unionSelect($table, bool $unionAll = false): QueryBuilder
     {
         if (empty($table)) {
             $this->setError('Empty $table in ' . __METHOD__);
-            return $this;
-        }
-
-        if (mb_strpos(mb_strtolower($this->sql), 'union') !== false) {
-            $this->setError('SQL has already UNION in ' . __METHOD__);
             return $this;
         }
 
@@ -1014,7 +1012,7 @@ class QueryBuilder
     }
 
     /**
-     * @return $this
+     * @return QueryBuilder
      */
     public function excepts(): QueryBuilder
     {
@@ -1024,8 +1022,8 @@ class QueryBuilder
     }
 
     /**
-     * @param $table
-     * @return $this
+     * @param string|array $table
+     * @return QueryBuilder
      */
     public function exceptSelect($table): QueryBuilder
     {
@@ -1053,7 +1051,7 @@ class QueryBuilder
     }
 
     /**
-     * @return $this
+     * @return QueryBuilder
      */
     public function intersect(): QueryBuilder
     {
@@ -1063,8 +1061,8 @@ class QueryBuilder
     }
 
     /**
-     * @param $table
-     * @return $this
+     * @param string|array $table
+     * @return QueryBuilder
      */
     public function intersectSelect($table): QueryBuilder
     {
@@ -1102,7 +1100,7 @@ class QueryBuilder
     /**
      * @param string $viewName
      * @param bool $addExists
-     * @return $this
+     * @return QueryBuilder
      */
     public function createView(string $viewName, bool $addExists = true): QueryBuilder
     {
@@ -1127,7 +1125,7 @@ class QueryBuilder
     /**
      * @param string $viewName
      * @param bool $addExists
-     * @return $this
+     * @return QueryBuilder
      */
     public function dropView(string $viewName, bool $addExists = true): QueryBuilder
     {
@@ -1148,7 +1146,7 @@ class QueryBuilder
 	/**
 	 * @param string $table
 	 * @param bool $addExists
-	 * @return $this
+	 * @return QueryBuilder
 	 */
 	public function drop(string $table, bool $addExists = true): QueryBuilder
 	{
@@ -1167,7 +1165,7 @@ class QueryBuilder
 
 	/**
 	 * @param string $table
-	 * @return $this
+	 * @return QueryBuilder
 	 */
 	public function truncate(string $table): QueryBuilder
 	{
@@ -1182,6 +1180,9 @@ class QueryBuilder
 		return $this;
 	}
 
+    /**
+     * @return string
+     */
     public function getDriver(): string
     {
         return strtolower($this->pdo->getAttribute(PDO::ATTR_DRIVER_NAME));
